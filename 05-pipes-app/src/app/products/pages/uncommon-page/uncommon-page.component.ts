a34749522956ac9202c449c2287f537275099110
adiaz <adiaz@nartexsoft.com>
2023-10-18 12:17:32 +0100

    Fin de la sección 11.

---
+++ 05-pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+
+import { Observable, interval, tap } from 'rxjs';
+
+type Gender = 'male' | 'female' | '';
+
+@Component({
+  selector: 'app-uncommon-page',
+  templateUrl: './uncommon-page.component.html',
+  styleUrls: ['./uncommon-page.component.css']
+})
+export class UncommonPageComponent {
+  // i18nSelect
+  public name:string = 'Alejandro';
+  public gender:Gender = 'male';
+  public invitationMap = {
+    male:'invitarlo',
+    female:'invitarla'
+  };
+
+  public changeClient():void {
+    this.name = 'Clara';
+    this.gender = 'female';
+  }
+
+  // i18nPlural | slice
+  public clients:string[] = ['María', 'Pedro', 'Fernando', 'Eduardo', 'Alejandro', 'Clara'];
+
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
+
+  public removeClient():void {
+    this.clients.shift();
+  }
+
+  // Json | KeyValue
+  public person ={
+    name: 'Alejandro',
+    age: 29,
+    address: 'S/C de Tenerife, Canarias'
+  }
+
+  // Async
+  public myObservableTimer:Observable<number> = interval(2000).pipe(
+    tap(value => console.log(`Tap: ${value}`))
+  );
+
+  public promiseValue:Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+      resolve('Obtenemos datos en la promesa')
+    }, 3000);
+  });
+}
---
+++ 05-pipes-app/src/app/products/products.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
+import { BasicsPageComponent } from './pages/basics-page/basics-page.component';
+import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+
+@NgModule({
+  declarations: [
+    BasicsPageComponent,
+    UncommonPageComponent,
+    NumbersPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    PrimeNgModule
+  ]
+})
+export class ProductsModule { }
